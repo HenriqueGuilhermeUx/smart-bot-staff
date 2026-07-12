45fe3c8c8f1b3e799ee48587adc50aa38ce72394
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-12 00:08:13 -0300

    Add Staff company information

---
+++ src/lib/company.ts
@@ -0,0 +1,7 @@
+export const COMPANY = {
+  legalName: 'Alternative Ventures Ltda',
+  cnpj: '61.920.356/0001-38',
+  emailUrl: 'mailto:henriquecampos66@gmail.com?subject=Contato%20Staff',
+  whatsappUrl: 'https://wa.me/5511947984328?text=Ol%C3%A1%2C%20quero%20falar%20sobre%20o%20Staff.',
+  websiteUrl: 'https://alternativeventures.com.br',
+} as const
