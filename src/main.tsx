9e54b8dfe9752b290c745466f8a0404e39f33193
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-20 00:28:27 -0300

    Install native Staff backend bridge before app startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import { installStaffNativeApiBridge } from './lib/staffApi.ts'
 import './index.css'
 import './family.css'
 import AppV2 from './AppV2.tsx'
+
+installStaffNativeApiBridge()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
